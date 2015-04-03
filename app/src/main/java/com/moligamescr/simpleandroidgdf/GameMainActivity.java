19f1863d4c3a8d788d46a9e5a3a8cae3fa8bf913
Gilberth Molina <gmolinac@outlook.com>
2015-04-03 00:25:05 -0600

    Initial commit
    Initial commit

---
+++ app/src/main/java/com/moligamescr/simpleandroidgdf/GameMainActivity.java
@@ -0,0 +1,22 @@
+package com.moligamescr.simpleandroidgdf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.res.AssetManager;
+
+public class GameMainActivity extends Activity {
+
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 450;
+    public static GameView sGame;
+    public static AssetManager assets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        assets = getAssets();
+        sGame = new GameView(this, GAME_WIDTH, GAME_HEIGHT);
+        setContentView(sGame);
+    }
+
+}
